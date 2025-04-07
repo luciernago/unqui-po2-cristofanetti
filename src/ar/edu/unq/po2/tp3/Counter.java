aef2a668170fd086636a9203b6f11785d466f577
luciernago <luciernago@DESKTOP-FFFMT93>
2025-04-06 22:11:48 -0300

    Clase Counter y CounterTestCase

---
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -0,0 +1,42 @@
+package ar.edu.unq.po2.tp3;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Counter {
+
+	private List<Integer> contador = new ArrayList<Integer>();
+
+	public Counter() {
+		super();
+	}
+	
+	public void addNumber(int x) {
+		this.contador.add(x);
+	}
+
+	public int cantidadDePares() {	
+		return cantidadMultiplosDe(2);
+	}
+	
+	public int cantidadDeImpares() {	
+		return contador.size() - cantidadMultiplosDe(2);
+	}
+	
+	public int cantidadMultiplosDe(int x) {
+		int suma = 0;
+		for(int elemento: contador) {
+			if (esMultiplo(elemento, x)) {
+				suma++;
+			}
+		}
+		return suma;
+	}
+	
+	private boolean esMultiplo(int x, int y) {
+		return x % y == 0;
+	}
+	
+	
+	
+}
---
+++ test/ar/edu/unq/po2/tp3/CounterTestCase.java
@@ -0,0 +1,64 @@
+package ar.edu.unq.po2.tp3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CounterTestCase {
+	
+	private Counter counter;
+	
+	/**
+	 * Crea un escenario de test básico,que consiste en un contador
+	 * con 10 enteros
+	 */
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		// se crea el contador
+		counter = new Counter();
+		
+		//se agregan los números. un solo par y nueve pares
+		counter.addNumber(1);
+		counter.addNumber(3);
+		counter.addNumber(5);
+		counter.addNumber(7);
+		counter.addNumber(9);
+		counter.addNumber(1);
+		counter.addNumber(1);
+		counter.addNumber(1);
+		counter.addNumber(1);
+		counter.addNumber(4);
+			
+	}
+	@Test
+	public void testCantidadDeImpares() {
+		// Obteniendo la cantidad de pares
+			int amount = counter.cantidadDeImpares();
+		
+		// Chequeo que la cantidad es la esperada
+			assertEquals(amount, 9);
+	}
+	
+	@Test
+	public void testCantidadDePares() {
+		// Obteniendo la cantidad de pares
+			int amount = counter.cantidadDePares();
+		
+		// Chequeo que la cantidad es la esperada
+			assertEquals(amount, 1);
+	}
+	
+	@Test
+	public void testCantidadDeMultilplosDe3() {
+		// Obteniendo la cantidad de pares
+			int amount = counter.cantidadMultiplosDe(3);
+		
+		// Chequeo que la cantidad es la esperada
+			assertEquals(amount, 2);
+	}
+
+
+
+}
