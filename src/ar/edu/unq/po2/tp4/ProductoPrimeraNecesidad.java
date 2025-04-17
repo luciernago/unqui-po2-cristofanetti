1c1dc259084f791f3999da416ee85ba5532dea3f
luciernago <luciernago@DESKTOP-FFFMT93>
2025-04-17 17:30:34 -0300

    Ejercicio 2

--- src/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidad.java
+++ src/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidad.java
@@ -1,6 +1,8 @@
 package ar.edu.unq.po2.tp4;
 
 public class ProductoPrimeraNecesidad extends Producto {
+	
+	private Double descuento = 0.9;
 
 	public ProductoPrimeraNecesidad (String s, Double p) {
 		super(s,p);
@@ -10,8 +12,21 @@ public class ProductoPrimeraNecesidad extends Producto {
 		super(s,p,c);
 	}
 	
+	public ProductoPrimeraNecesidad (String s, Double p, boolean c, Double d) {
+		super(s,p,c);
+		this.setDescuento(d);
+	}
+	
+	public void setDescuento(Double n) {
+		this.descuento = (100d - n) *0.01d; 
+	}
+	
+	public Double getDescuento() {
+		return this.descuento;
+	}
+	
 	public Double getPrecio() {
-		return super.getPrecio() * 0.9;
+		return super.getPrecio() * this.getDescuento();
 	}
 	
 }
--- test/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidadTest.java
+++ test/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidadTest.java
@@ -8,14 +8,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ProductoPrimeraNecesidadTest {
 
 	private ProductoPrimeraNecesidad leche;
+	private ProductoPrimeraNecesidad arroz;
 	
 	@BeforeEach
 	public void setUp() {
 		leche = new ProductoPrimeraNecesidad("Leche", 8d, false);
+		arroz = new ProductoPrimeraNecesidad("Arroz", 10d, true, 15d);
 	}
 	
 	@Test
 	public void testCalcularPrecio() {
 		assertEquals(new Double(7.2), leche.getPrecio());
+		assertEquals(new Double(8.5), arroz.getPrecio());
 	}
 }
